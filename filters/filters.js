ab94ce867069e32e34e2ccf0f5982e65652455ee
Sahaj Kulshrestha <kulshresthasahaj@gmail.com>
2020-01-06 09:58:48 +0530

    full

---
+++ filters/filters.js
@@ -0,0 +1,128 @@
+//Grayscale
+var image = null;
+
+function upload() {
+  //Get input from file input
+  var fileinput = document.getElementById("finput");
+  //Make new SimpleImage from file input
+  image = new SimpleImage(fileinput);
+  //Get canvas
+  var canvas = document.getElementById("can");
+  //Draw image on canvas
+  image.drawTo(canvas);
+}
+
+function makeGray() {
+  //change all pixels of image to gray
+  for (var pixel of image.values()) {
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    pixel.setRed(avg);
+    pixel.setGreen(avg);
+    pixel.setBlue(avg);
+  }
+  //display new image
+  var canvas = document.getElementById("can");
+  image.drawTo(canvas);
+}
+
+
+//Rainbow
+function filterRainbow() {
+  var height = image.getHeight();
+  for (var pixel of image.values()) {
+    var y = pixel.getY();
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    if (y < height / 7) {
+      //red
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 2 / 7) {
+      //orange
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(0.8 * avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(1.2 * avg - 51);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 3 / 7) {
+      //yellow
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(2 * avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 4 / 7) {
+      //green
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(2 * avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(2 * avg - 255);
+        pixel.setGreen(255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 5 / 7) {
+      //blue
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(2 * avg);
+      } else {
+        pixel.setRed(2 * avg - 255);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(255);
+      }
+    } else if (y < height * 6 / 7) {
+      //indigo
+      if (avg < 128) {
+        pixel.setRed(.8 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(2 * avg);
+      } else {
+        pixel.setRed(1.2 * avg - 51);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(255);
+      }
+    } else {
+      //violet
+      if (avg < 128) {
+        pixel.setRed(1.6 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(1.6 * avg);
+      } else {
+        pixel.setRed(0.4 * avg + 153);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(0.4 * avg + 153);
+      }
+    }
+  }
+  //display new image
+  var canvas = document.getElementById("can");
+  image.drawTo(canvas);
+}
+
+
+//Clear Canvas
+function clearCanvas() {
+  doClear(can);
+}
+
+function doClear(canvas) {
+  var context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
